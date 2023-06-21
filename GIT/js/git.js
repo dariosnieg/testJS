ca9d5768b046f67b7bf5d0b5869fd0f357a0d6fd
dariosnieg <dariosnieg@gmail.com>
2023-06-21 17:50:44 -0300

    subo nuevamente

--- GIT/js/git.js
+++ GIT/js/git.js
@@ -14,7 +14,17 @@ git init
 
 /////////////
 
-"Git add --all" o "Git add ." -> todo lo que está en mi proyecto y no se subió, lo sube al stage
+"Git add --all" o "Git add . " -> todo lo que está en mi proyecto y no se subió, lo sube al stage
+"git add Nombre_archivo " -> subo al stage ese único archivo
+
+para commitear
+git commit -m "{mensaje de lo commiteado}"
+
+Todo lo commiteado fue al stage
+
+----
+Puedo hacer un restore para volver los cambios hacia atras.
+"git restore {nombre_archivo}"
 
 
 */
