c73d426228fed87330b5522c44649594d41bbe42
dariosnieg <dariosnieg@gmail.com>
2023-06-29 17:30:40 -0300

    TP2 con listadito

---
+++ TP2/js/script.js
@@ -0,0 +1,73 @@
+const agregarTask = document.getElementById("agregarTask");
+
+var arrayTask = [];
+var objeto = {};
+
+function agregoElementoArray(task){
+    arrayTask.push(task);
+}
+
+function eliminoElementoArray(posicion){
+    arrayTask.pop();
+}
+
+agregarTask.addEventListener('click', () => {
+    var newTask = document.getElementById('task');
+    //agregarNewTask();
+        objeto = {};
+        objeto.task = newTask.value;
+        objeto.timeStamp = Date.now();
+        objeto.timeDelete = '';
+        console.log(objeto);
+        agregoElementoArray(objeto);
+    
+        borroListado();
+        cargarTaskList();        
+        borroInput();
+        //resetListado();
+    
+});
+
+function borroInput(){
+    document.getElementById('task').value = "";
+    console.log('borro input');
+    document.getElementById('task').focus();
+};
+
+
+function cargarTaskList(){
+    var elementoCreado = "";
+    console.log('entro a cargarTaskList');
+    arrayTask.forEach(element => {
+        console.log('Elemento:' + element.task);
+        elementoCreado = document.createElement("a");
+        elementoCreado.setAttribute('class', 'list-group-item list-group-item-action taskElement text-decoration-line-through');
+
+        
+        var s = document.createElement('s');
+        s.innerHTML = element.task;
+
+        document.getElementById('listadoTask').appendChild(elementoCreado).appendChild(s);
+//        var s = elementoCreado.createElement("s");
+        // elementoCreado.innerHTML = element.task;
+    });
+    
+}
+
+function resetListado(){
+    borroListado();
+    arrayTask = [];
+    document.getElementById('task').focus();   
+}
+
+function borroListado(){
+    var Lista = document.getElementById('listadoTask');
+
+    while (Lista.hasChildNodes()) {
+        console.log('Borro:' + Lista.firstChild.value);
+        Lista.removeChild(Lista.firstChild);
+    }
+    if(Lista.hasChildNodes()==false){
+        console.log('No hay mas elementos en la lista');
+    }
+}
