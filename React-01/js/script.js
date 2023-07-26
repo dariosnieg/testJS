e2630b024b9576353e2d88e21eb1789099f670d4
dariosnieg <dariosnieg@gmail.com>
2023-07-25 22:24:50 -0300

    React-01

---
+++ React-01/js/script.js
@@ -0,0 +1,10 @@
+// Render -> renderiza el elemento Raiz, el componente que quiero mostrar en pantalla.
+// Voy a poder crear arbol de componentes
+// Por lo general renderiza en un DIV
+
+
+
+// Los componentes tienen propiedades, parámetros que les puedo pasar a mis componentes
+
+//condición IF en linea -> condicion ? opcionTrue : opcionFalse
+// Ej: edad>18 ? dejarpasar() : prohibirEntrada()
